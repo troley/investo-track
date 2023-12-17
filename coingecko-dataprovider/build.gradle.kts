604d832fcd5d914adc9d1db16d960de839c20c57
rene <rene.uhliar@gmail.com>
2023-12-17 19:27:01 +0100

    CoinGecko Gradle: add integration test sourceset

--- coingecko-dataprovider/build.gradle.kts
+++ coingecko-dataprovider/build.gradle.kts
@@ -5,6 +5,21 @@ plugins {
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
 }
+
+sourceSets {
+	create("intTest") {
+		compileClasspath += sourceSets.main.get().output
+		runtimeClasspath += sourceSets.main.get().output
+	}
+}
+
+val intTestImplementation: Configuration by configurations.getting {
+	extendsFrom(configurations.testImplementation.get())
+}
+val intTestRuntimeOnly: Configuration by configurations.getting
+
+configurations["intTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux:3.2.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.0")
