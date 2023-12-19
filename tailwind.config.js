87ba6a7856b895b8a5a804a516d40e7410aed454
rene <rene.uhliar@gmail.com>
2023-12-19 14:19:21 +0100

    Add & configure DaisyUI dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["dark"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
