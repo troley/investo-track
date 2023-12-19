f5c332502803fbc9e41953e2f449028282f9f651
rene <rene.uhliar@gmail.com>
2023-12-19 09:56:05 +0100

    Introduce caching of the large response

--- webapp/src/main/java/com/example/investotrack/webapp/service/CurrencyDataProviderServiceImpl.java
+++ webapp/src/main/java/com/example/investotrack/webapp/service/CurrencyDataProviderServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.investotrack.webapp.service;
 import com.example.investotrack.dataprovidercore.CurrencyDataProvider;
 import com.example.investotrack.webapp.viewmodel.CurrencyViewModel;
 import com.example.investotrack.webapp.viewmodel.convertor.CurrencyModelConvertor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -19,6 +20,7 @@ class CurrencyDataProviderServiceImpl implements CurrencyDataProviderService {
     }
 
     @Override
+    @Cacheable("currencies")
     public Collection<CurrencyViewModel> listAllCurrencies() {
         return currencyDataProvider.listAllCurrencies().stream().map(convertor::toViewModel).toList();
     }
