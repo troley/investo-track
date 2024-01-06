77cddd440169a9cb7a0905e9134ae5622cb14a59
rene <rene.uhliar@gmail.com>
2024-01-06 22:59:03 +0100

    BE CurrencyViewModel: make serializable to enable storing in Redis

--- webapp/src/main/java/com/example/investotrack/webapp/viewmodel/CurrencyViewModel.java
+++ webapp/src/main/java/com/example/investotrack/webapp/viewmodel/CurrencyViewModel.java
@@ -1,3 +1,5 @@
 package com.example.investotrack.webapp.viewmodel;
 
-public record CurrencyViewModel(String id, String symbol, String name) {}
+import java.io.Serializable;
+
+public record CurrencyViewModel(String id, String symbol, String name) implements Serializable {}
