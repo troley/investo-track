9c2bbd52b7f5134f0ab17d36956b888d542adc35
rene <rene.uhliar@gmail.com>
2024-01-07 10:47:30 +0100

    BE CurrencyDataProviderServiceImplTest: adjust tests to new situation

--- webapp/src/test/java/com/example/investotrack/webapp/service/CurrencyDataProviderServiceImplTest.java
+++ webapp/src/test/java/com/example/investotrack/webapp/service/CurrencyDataProviderServiceImplTest.java
@@ -1,31 +1,38 @@
 package com.example.investotrack.webapp.service;
 
 import com.example.investotrack.dataprovidercore.CurrencyDataProvider;
-import com.example.investotrack.dataprovidercore.model.AbstractCurrency;
+import com.example.investotrack.webapp.datastore.DataCache;
 import com.example.investotrack.webapp.viewmodel.CurrencyViewModel;
 import com.example.investotrack.webapp.viewmodel.convertor.CurrencyModelConvertor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CurrencyDataProviderServiceImplTest {
 
+    // Unused but required by CurrencyDataProviderServiceImpl
     @Mock
     private CurrencyDataProvider currencyDataProvider;
 
+    // Unused but required by CurrencyDataProviderServiceImpl
     @Mock
     private CurrencyModelConvertor convertor;
+
+    @Mock
+    private DataCache dataCache;
 
     @InjectMocks
     private CurrencyDataProviderServiceImpl currencyDataProviderService;
@@ -33,11 +40,12 @@ class CurrencyDataProviderServiceImplTest {
     @Test
     void listAllCurrencies_returnsCurrency() {
         // Arrange
-        AbstractCurrency btc = new AbstractCurrency("btc", "btc", "Bitcoin") {};
-        CurrencyViewModel viewModel = new CurrencyViewModel(btc.getId(), btc.getSymbol(), btc.getName());
+        CurrencyViewModel viewModel = new CurrencyViewModel("btc", "btc", "Bitcoin");
 
-        when(currencyDataProvider.listAllCurrencies()).thenAnswer((invocationOnMock -> List.of(btc)));
-        when(convertor.toViewModel(any(AbstractCurrency.class))).thenReturn(viewModel);
+        when(dataCache.getOrSupply(anyString(),
+                                   anyString(),
+                                   ArgumentMatchers.<Supplier<Collection<CurrencyViewModel>>>any())).thenReturn(List.of(
+                viewModel));
 
         // Act
         Collection<CurrencyViewModel> result = currencyDataProviderService.listAllCurrencies();
@@ -49,13 +57,14 @@ class CurrencyDataProviderServiceImplTest {
     @Test
     void listAllCurrencies_returnsTwoCurrencies() {
         // Arrange
-        AbstractCurrency btc = new AbstractCurrency("btc", "btc", "Bitcoin") {};
-        AbstractCurrency eth = new AbstractCurrency("eth", "eth", "Ethereum") {};
-        CurrencyViewModel btcViewModel = new CurrencyViewModel(btc.getId(), btc.getSymbol(), btc.getName());
-        CurrencyViewModel ethViewModel = new CurrencyViewModel(eth.getId(), eth.getSymbol(), eth.getName());
+        CurrencyViewModel btcViewModel = new CurrencyViewModel("btc", "btc", "Bitcoin");
+        CurrencyViewModel ethViewModel = new CurrencyViewModel("eth", "eth", "Ethereum");
 
-        when(currencyDataProvider.listAllCurrencies()).thenAnswer((invocationOnMock -> List.of(btc, eth)));
-        when(convertor.toViewModel(any(AbstractCurrency.class))).thenReturn(btcViewModel, ethViewModel);
+        when(dataCache.getOrSupply(anyString(),
+                                   anyString(),
+                                   ArgumentMatchers.<Supplier<Collection<CurrencyViewModel>>>any())).thenReturn(List.of(
+                btcViewModel,
+                ethViewModel));
 
         // Act
         Collection<CurrencyViewModel> result = currencyDataProviderService.listAllCurrencies();
@@ -67,7 +76,9 @@ class CurrencyDataProviderServiceImplTest {
     @Test
     void listAllCurrencies_noData_returnsEmptyList() {
         // Arrange
-        when(currencyDataProvider.listAllCurrencies()).thenAnswer((invocationOnMock -> List.of()));
+        when(dataCache.getOrSupply(anyString(),
+                                   anyString(),
+                                   ArgumentMatchers.<Supplier<Collection<CurrencyViewModel>>>any())).thenReturn(List.of());
 
         // Act
         Collection<CurrencyViewModel> result = currencyDataProviderService.listAllCurrencies();
@@ -79,21 +90,16 @@ class CurrencyDataProviderServiceImplTest {
     @Test
     void findCaseInsensitiveCurrenciesBy_randomCaseNoMatch_returnsEmptyList() {
         // Arrange
-        var btc = new AbstractCurrency("btc", "btc", "Bitcoin") {
-        };
-        var btcLite = new AbstractCurrency("btcLite", "btclite", "Bitcoin Lite") {
-        };
-        var eth = new AbstractCurrency("eth", "eth", "Ethereum") {
-        };
-
         var btcViewModel = new CurrencyViewModel("btc", "btc", "Bitcoin");
         var btcLiteViewModel = new CurrencyViewModel("btcLite", "btclite", "Bitcoin Lite");
         var ethViewModel = new CurrencyViewModel("eth", "eth", "Ethereum");
 
-        when(currencyDataProvider.listAllCurrencies()).thenAnswer((invocationOnMock -> List.of(btc, btcLite, eth)));
-        when(convertor.toViewModel(any(AbstractCurrency.class))).thenReturn(btcViewModel,
-                                                                            btcLiteViewModel,
-                                                                            ethViewModel);
+        when(dataCache.getOrSupply(anyString(),
+                                   anyString(),
+                                   ArgumentMatchers.<Supplier<Collection<CurrencyViewModel>>>any())).thenReturn(List.of(
+                btcViewModel,
+                btcLiteViewModel,
+                ethViewModel));
 
         // Act
         Collection<CurrencyViewModel> result = currencyDataProviderService.findCaseInsensitiveCurrenciesBy("dOgE");
@@ -105,21 +111,16 @@ class CurrencyDataProviderServiceImplTest {
     @Test
     void findCaseInsensitiveCurrenciesBy_randomCaseMatch_returnsMatchingCoins() {
         // Arrange
-        var btc = new AbstractCurrency("btc", "btc", "Bitcoin") {
-        };
-        var btcLite = new AbstractCurrency("btcLite", "btclite", "Bitcoin Lite") {
-        };
-        var eth = new AbstractCurrency("eth", "eth", "Ethereum") {
-        };
-
         var btcViewModel = new CurrencyViewModel("btc", "btc", "Bitcoin");
         var btcLiteViewModel = new CurrencyViewModel("btcLite", "btclite", "Bitcoin Lite");
         var ethViewModel = new CurrencyViewModel("eth", "eth", "Ethereum");
 
-        when(currencyDataProvider.listAllCurrencies()).thenAnswer((invocationOnMock -> List.of(btc, btcLite, eth)));
-        when(convertor.toViewModel(any(AbstractCurrency.class))).thenReturn(btcViewModel,
-                                                                            btcLiteViewModel,
-                                                                            ethViewModel);
+        when(dataCache.getOrSupply(anyString(),
+                                   anyString(),
+                                   ArgumentMatchers.<Supplier<Collection<CurrencyViewModel>>>any())).thenReturn(List.of(
+                btcViewModel,
+                btcLiteViewModel,
+                ethViewModel));
 
         // Act
         Collection<CurrencyViewModel> result = currencyDataProviderService.findCaseInsensitiveCurrenciesBy("bTc");
