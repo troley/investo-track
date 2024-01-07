04826ac2762d3b68fe630b4f18d07c228903b0af
rene <rene.uhliar@gmail.com>
2024-01-07 20:11:11 +0100

    BE Gradle: move sourceSets config from coingecko-dataprovider to parent

--- coingecko-dataprovider/build.gradle.kts
+++ coingecko-dataprovider/build.gradle.kts
@@ -8,20 +8,6 @@ java {
 	}
 }
 
-sourceSets {
-	create("intTest") {
-		compileClasspath += sourceSets.main.get().output
-		runtimeClasspath += sourceSets.main.get().output
-	}
-}
-
-val intTestImplementation: Configuration by configurations.getting {
-	extendsFrom(configurations.testImplementation.get())
-}
-val intTestRuntimeOnly: Configuration by configurations.getting
-
-configurations["intTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
-
 dependencies {
 	implementation(project(":dataprovider-core"))
 
